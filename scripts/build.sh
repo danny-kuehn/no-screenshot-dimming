a91e6a7507da88551ce0b5ddfe333dbaf26dee42
Daniel Kuehn <daniel@kuehn.foo>
2025-11-08 07:11:02 -0500

    chore: run shfmt on script file

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,19 +11,19 @@ gnome-extensions pack src --force --extra-source="../LICENSE"
 echo >&2 "Packed."
 
 while getopts i flag; do
-	case $flag in
-	i)
-		echo >&2 "Installing extension..."
+  case $flag in
+    i)
+      echo >&2 "Installing extension..."
 
-		gnome-extensions install --force \
-			no-screenshot-dimming@danny-kuehn-on-github.shell-extension.zip
+      gnome-extensions install --force \
+        no-screenshot-dimming@danny-kuehn-on-github.shell-extension.zip
 
-		echo >&2 "Installed. Reload GNOME Shell."
-		;;
-	*)
-		echo >&2 "Usage: $0 [-i]"
-		echo >&2 "  -i   Install after building"
-		exit 1
-		;;
-	esac
+      echo >&2 "Installed. Reload GNOME Shell."
+      ;;
+    *)
+      echo >&2 "Usage: $0 [-i]"
+      echo >&2 "  -i   Install after building"
+      exit 1
+      ;;
+  esac
 done
